a1986443bfb5d413717b5e2cb44c50a55018bacb
Ömer Faruk Özbek <omer.ozbek.faruk@gmail.com>
2021-12-28 22:37:21 +0300

    Button is added for throwing dice.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var randomNumber1 = Math.floor(Math.random()*6 + 1); //Random number between 1-6 for dice 1.
-var randomNumber2 = Math.floor(Math.random()*6 + 1); //Random number between 1-6 for dice 2.
+document.querySelector('button').addEventListener('click', function(){
+  var randomNumber1 = Math.floor(Math.random()*6 + 1); //Random number between 1-6 for dice 1.
+  var randomNumber2 = Math.floor(Math.random()*6 + 1); //Random number between 1-6 for dice 2.
 
-var randomImage1 = "images/dice" + randomNumber1 + ".png"; //The random image for dice 1.
-var randomImage2 = "images/dice" + randomNumber2 + ".png"; //The random image for dice 2.
+  var randomImage1 = "images/dice" + randomNumber1 + ".png"; //The random image for dice 1.
+  var randomImage2 = "images/dice" + randomNumber2 + ".png"; //The random image for dice 2.
 
-document.getElementsByTagName("img")[0].setAttribute("src", randomImage1); // Change the image.
-document.getElementsByTagName("img")[1].setAttribute("src", randomImage2); // Change the image.
+  document.getElementsByTagName("img")[0].setAttribute("src", randomImage1); // Change the image.
+  document.getElementsByTagName("img")[1].setAttribute("src", randomImage2); // Change the image.
 
-if(randomNumber1 > randomNumber2){ // Change the title and show who wins.
-  document.querySelector("h1").textContent = "🏆 Player1 Wins!";
-}
-else if(randomNumber1 < randomNumber2){
-  document.querySelector("h1").textContent = "Player2 Wins! 🏆";
-}
-else{
-  document.querySelector("h1").textContent = "Draw!";
-}
+  if(randomNumber1 > randomNumber2){ // Change the title and show who wins.
+    document.querySelector("h1").textContent = "🏆 Player1 Wins!";
+  }
+  else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent = "Player2 Wins! 🏆";
+  }
+  else{
+    document.querySelector("h1").textContent = "Draw!";
+  }
+});
